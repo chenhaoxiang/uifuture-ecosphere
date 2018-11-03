a2c12b050a474c2def3dfb3c17fca77d088684bd
chenhaoxiang <chenhaoxiang0117@qq.com>
2018-11-03 17:48:09 +0800

    创建项目

---
+++ uifuture-ecosphere/src/main/java/com/uifuture/ecosphere/UifutureEcosphereApplication.java
@@ -0,0 +1,12 @@
+package com.uifuture.ecosphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UifutureEcosphereApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UifutureEcosphereApplication.class, args);
+    }
+}
---
+++ uifuture-ecosphere/src/test/java/com/uifuture/ecosphere/UifutureEcosphereApplicationTests.java
@@ -0,0 +1,16 @@
+package com.uifuture.ecosphere;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class UifutureEcosphereApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
